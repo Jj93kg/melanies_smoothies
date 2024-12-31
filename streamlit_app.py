0997f25b286f1be4b03f019f7ac9ed4cbc552f84
JJ Daley <109515804+Jj93kg@users.noreply.github.com>
2024-12-31 11:06:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert to pandas dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd, df)
+st.dataframe(pd_df)
 st.stop
 
 ingredients_list = st.multiselect(
